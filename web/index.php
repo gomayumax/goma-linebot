5a3596e583da476df48913337119b9d5f51dda73
ごま <gomayumax@users.noreply.github.com>
2016-04-16 16:41:58 +0900

    yahoo apiを試しに使ってみる

--- web/index.php
+++ web/index.php
@@ -9,33 +9,53 @@ $app = new Silex\Application();
 $bot = new CU\LineBot();
 
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => 'php://stderr',
+  'monolog.logfile' => 'php://stderr',
 ));
 
 $app->before(function (Request $request) use($bot) {
-    // Signature validation
-    $request_body = $request->getContent();
-    $signature = $request->headers->get('X-LINE-CHANNELSIGNATURE');
-    if (!$bot->isValid($signature, $request_body)) {
-        return new Response('Signature validation failed.', 400);
-    }
+  // Signature validation
+  $request_body = $request->getContent();
+  $signature = $request->headers->get('X-LINE-CHANNELSIGNATURE');
+  if (!$bot->isValid($signature, $request_body)) {
+    return new Response('Signature validation failed.', 400);
+  }
 });
 
 $app->post('/callback', function (Request $request) use ($app, $bot) {
-    // Let's hack from here!
-    $body = json_decode($request->getContent(), true);
+  // Let's hack from here!
+  $body = json_decode($request->getContent(), true);
 
-    foreach ($body['result'] as $obj) {
-        $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
-        $from = $obj['content']['from'];
-        $content = $obj['content'];
+  foreach ($body['result'] as $obj) {
+    $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
+    $from = $obj['content']['from'];
+    $content = $obj['content'];
 
-        if ($content['text']) {
-            $bot->sendText($from, sprintf('%sじゃない', $content['text'])); 
-        }
+    if ($content['text']) {
+      $pos = $this->parseString($content['text']);
+      //$bot->sendText($from, sprintf('%sじゃない', $content['text'])); 
+      $bot->sendText($from, sprintf('%sじゃない', $pos)); 
     }
+  }
 
-    return 0;
+  return 0;
 });
 
 $app->run();
+
+
+
+public function parseString($text) {
+  $query = "$text";
+  $apiKey = "dj0zaiZpPUFUaUtaWHZaQmc3QyZzPWNvbnN1bWVyc2VjcmV0Jng9NjE-";
+
+  $url = "http://jlp.yahooapis.jp/MAService/V1/parse?appid=" . $apiKey . "&sentence=" . $query;
+
+  $rss = file_get_contents($url);
+  $xml = simplexml_load_string($rss);
+
+  foreach($xml->ma_result->word_list->word as $item) {
+    //echo $item->surface . " " . $item->pos;
+    //echo "<br />";
+    return $item->pos;
+  } 
+}
